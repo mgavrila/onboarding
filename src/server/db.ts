ad4114efc53a716de2d4c35a79f6f22d4bfa2336
Andrei Gavrila <andrei.gavrila94@yahoo.com>
2023-02-06 16:10:21 +0200

    Init project

---
+++ src/server/db.ts
@@ -0,0 +1,35 @@
+import { MongoClient } from "mongodb";
+
+import { env } from "../env/server.mjs";
+
+if (!env.DATABASE_URL) {
+  throw new Error('Invalid/Missing environment variable: "MONGODB_URI"');
+}
+
+const uri = env.DATABASE_URL;
+const options = {};
+
+let client;
+let clientPromise: Promise<MongoClient>;
+
+const global = globalThis as unknown as {
+  _mongoClientPromise: Promise<MongoClient> | null;
+};
+
+if (process.env.NODE_ENV === "development") {
+  // In development mode, use a global variable so that the value
+  // is preserved across module reloads caused by HMR (Hot Module Replacement).
+  if (!global._mongoClientPromise) {
+    client = new MongoClient(uri, options);
+    global._mongoClientPromise = client.connect();
+  }
+  clientPromise = global._mongoClientPromise;
+} else {
+  // In production mode, it's best to not use a global variable.
+  client = new MongoClient(uri, options);
+  clientPromise = client.connect();
+}
+
+// Export a module-scoped MongoClient promise. By doing this in a
+// separate module, the client can be shared across functions.
+export default clientPromise;
